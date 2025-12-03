d17243f84fa00ee2e9b03534b5e080451a5b996c
Ayush Poudel <ayushpoudel@usf.edu>
2025-12-03 15:28:10 -0500

    -madd server init, db init and jwt init

--- main.go
+++ main.go
@@ -4,5 +4,17 @@ var host = "localhost"
 var port = "9000"
 
 func main() {
+	dbErr := db.InitializeDB()
+	if dbErr != nil {
+		panic(dbErr)
+	}
+	jwtErr := myJwt.InitializeJWT()
+	if jwtErr != nil {
+		panic(jwtErr)
+	}
+	serverErr := server.StartServer(host, port)
+	if serverErr != nil {
+		panic(serverErr)
+	}
 
 }
