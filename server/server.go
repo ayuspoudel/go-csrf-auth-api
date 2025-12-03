8316a3311a7e98d88b2e1808ee80a2a0fd6c8333
Ayush Poudel <ayushpoudel@usf.edu>
2025-12-03 15:44:37 -0500

    Add server code to initialize and start server based on host and port using net/http go package

--- server/server.go
+++ server/server.go
@@ -1,1 +1,17 @@
 package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func StartServer(hostName string, port string) error {
+	host := hostName + ":" + port
+	fmt.Printf("listening on: %s", host)
+
+	handler := middleware.NewHandler()
+
+	http.Handle("/", handler)
+	return http.ListenAndServe(host, nil)
+
+}
