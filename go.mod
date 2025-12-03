c4f86a46b37cd44aab3033c09ae242405ed49eb6
Ayush Poudel <ayushpoudel@usf.edu>
2025-12-03 15:51:26 -0500

    Add middleware code for logic handler, auth handler and recover handler

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ayuspoudel/go-csrf-auth-api
 
 go 1.24.3
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/justinas/alice v1.2.0 // indirect
+)
--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,1 +1,52 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
+
+func NewHandler() http.Handler {
+	return alice.New(recoverHandler, authHandler).ThenFunc(logicHandler)
+}
+
+func recoverHandler(next http.Handler) http.Handler {
+
+}
+
+func authHandler(next http.Handler) http.Handler {
+
+}
+
+func logicHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/restricted":
+	case "/login":
+		switch r.Method {
+		case "POST":
+		case "GET":
+		default:
+		}
+	case "/register":
+		switch r.Method {
+		case "POST":
+		case "GET":
+		default:
+		}
+	case "/logout":
+	case "/deleteUser":
+	default:
+	}
+}
+
+func nullifyTokenCookies(w *http.ResponseWriter, r *http.Request) {
+
+}
+
+func setAuthAndRefreshCookies() {
+
+}
+
+func grabCsrfFromRequest(r *http.Request) string {
+
+}
